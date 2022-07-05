5ab5a33a786afd877c06e9c153355f221e635f98
Ali Zein Al Dine <alized559@gmail.com>
2022-07-05 23:54:14 +0300

    add expert_options

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,12 +5,14 @@ import Footer from '../common/footer';
 import BuiltExclusively from './built_exclusively';
 import JoinUs from './join_us';
 import DarkLightModes from './dark_light_modes';
+import ExportOptions from './export_options';
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <FirstPage />
+      <ExportOptions />
       <DarkLightModes />
       <BuiltExclusively />
       <JoinUs />
