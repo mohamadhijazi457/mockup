93fda24791bdb2b7671b1d41b8e65ddf4820797e
mohamadhijazi457 <mohamadhijazi457@gmail.com>
2022-07-07 14:44:42 +0300

    sv

--- src/components/home/diversetemplates.js
+++ src/components/home/diversetemplates.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Styled from 'styled-components';
 import RightSideImg from '../../images/right group devices.png';
-import LeftSideImg from '../../images/left group devices 1.png';
 
 const DiverseTemplates = () => {
     const [screenWidth, setScreenWidth] = useState(0);
@@ -26,19 +25,27 @@ const DiverseTemplates = () => {
             <TextMiddle>
             <p style={{fontFamily:"HelveticaNeue-Medium", fontSize:"30px"}}>Over 35 diverse<br/> templates on hand</p>
             <p style={{fontFamily:"HelveticaNeue-Regular",fontSize:"18px",paddingTop:"30px"}}>Mockup covers various templates to help you envision your project<br/> within its intended frame. It offers more than 35 distinct options to<br/> choose from including:</p>
-            <div class="List_Services">
-              <ul style={{fontFamily:"HelveticaNeue-Regular", fontSize:"20px", paddingTop:"20px"}}>
-              <li style={{listStyle:"none",display:"inline"}}>iPhone</li>
-              <li style={{listStyle:"none",display:"inline"}}>&nbsp; &nbsp;iPad</li>
-              <li style={{listStyle:"none",display:"inline"}}>&nbsp; &nbsp;Apple Watch</li>
-              <li style={{listStyle:"none",display:"inline"}}>&nbsp; &nbsp;Web</li>
-              <li style={{listStyle:"none",display:"inline"}}>&nbsp; &nbsp;Mac</li>
-              <li style={{listStyle:"none",display:"inline"}}>&nbsp; &nbsp;Android</li>
-              <li style={{listStyle:"none",display:"inline"}}>&nbsp; &nbsp;Widgets</li>
-              <li style={{listStyle:"none",display:"inline"}}>&nbsp; &nbsp;App Icon</li>
-              <li style={{listStyle:"none",display:"inline"}}>&nbsp; &nbsp;Blank template</li>
+              <ListServices>
+              <div>
+              <ul style={{fontFamily:"HelveticaNeue-Regular", fontSize:"14px",marginBottom:"10px"}}>
+              <li style={{listStyle:"none",display:"inline", border: "1px solid black", width:"16px",height:"12px",borderRadius:"10px",textAlign:"center",paddingLeft:"5px",paddingRight:"7px",marginRight:"5px"}}>iPhone</li>
+              <li style={{listStyle:"none",display:"inline", border: "1px solid black", width:"16px",height:"12px",borderRadius:"10px",textAlign:"center",paddingLeft:"2px",paddingRight:"9px",marginRight:"5px"}}>&nbsp; &nbsp;iPad</li>
+              <li style={{listStyle:"none",display:"inline", border: "1px solid black", width:"16px",height:"12px",borderRadius:"10px",textAlign:"center",paddingLeft:"0px",paddingRight:"9px",marginRight:"5px"}}>&nbsp; &nbsp;Apple Watch</li>
+              <li style={{listStyle:"none",display:"inline", border: "1px solid black", width:"16px",height:"12px",borderRadius:"10px",textAlign:"center",paddingLeft:"0px",paddingRight:"9px",marginRight:"5px"}}>&nbsp; &nbsp;Web</li>
+              <li style={{listStyle:"none",display:"inline", border: "1px solid black", width:"16px",height:"12px",borderRadius:"10px",textAlign:"center",paddingLeft:"0px",paddingRight:"9px",marginRight:"5px"}}>&nbsp; &nbsp;Mac</li>
+              <li style={{listStyle:"none",display:"inline", border: "1px solid black", width:"16x",height:"12px",borderRadius:"10px",textAlign:"center",paddingLeft:"0px",paddingRight:"9px",marginRight:"5px"}}>&nbsp; &nbsp;Android</li>
               </ul>
-            </div>
+              </div>
+              
+              <div>
+              <ul style={{fontFamily:"HelveticaNeue-Regular", fontSize:"14px",display:"inline",marginTop:"20px"}}>
+              <li style={{listStyle:"none",display:"inline", border: "1px solid black", width:"12px",height:"9px",borderRadius:"10px",textAlign:"center",paddingLeft:"0px",paddingRight:"9px",marginRight:"5px"}}>&nbsp; &nbsp;Widgets</li>
+              <li style={{listStyle:"none",display:"inline", border: "1px solid black", width:"16px",height:"12px",borderRadius:"10px",textAlign:"center",paddingLeft:"0px",paddingRight:"9px",marginRight:"5px"}}>&nbsp; &nbsp;App Icon</li>
+              <li style={{listStyle:"none",display:"inline", border: "1px solid black", width:"16px",height:"12px",borderRadius:"10px",textAlign:"center",paddingLeft:"5px",paddingRight:"9px",marginRight:"5px"}}>&nbsp; &nbsp;Blank template</li>
+              </ul>
+              </div>
+              
+              </ListServices>
             </TextMiddle>
             <RightSide>
             <img src={RightSideImg} alt='RightSide_png' style={{width:"400px", height:"450px"}} />
@@ -58,11 +65,10 @@ const DiverseTemplates = () => {
         const TextMiddle = Styled.div`
         padding-top:90px;
         
-        > List_Services{
-          list-style:none;
-        }
         
-        
+        `;
+        const ListServices = Styled.div`
+        padding-top:20px;
         `;
         const LeftSide = Styled.div`
         
