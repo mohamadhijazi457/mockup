b960f86b2ec2c16d7b586c6f89512948ec0bf1eb
Ali Zein Al Dine <alized559@gmail.com>
2022-07-09 14:21:12 +0300

    canvas clean code

--- src/components/home/canvasmobile.js
+++ src/components/home/canvasmobile.js
@@ -8,13 +8,16 @@ const CanvasMobile = () => {
 		<FirstContainer>
 			<p style={{fontFamily:"HelveticaNeue-Medium.ttf", fontSize:"42px",paddingLeft:"1px",textAlign:"center",paddingBottom:"60px",paddingTop:"70px"}}>
 				Flexible Canvas &<br/> slick Quick Actions</p>
-			<img src={LeftSideImg} alt='LeftSide_png' style={{width:"317.15px", height:"415.23px", paddingLeft:"40px",paddingBottom:"45px"}} />
-			<img src={RightSideImg} alt='RightSide_png' style={{width:"317.15px", height:"415.23px", paddingLeft:"40px", paddingRight:"150px",paddingBottom:"45px"}}/>
+			<img src={LeftSideImg} alt='LeftSide_png' style={{width:"317.15px", height:"415.23px",paddingBottom:"20px"}} />
+			<img src={RightSideImg} alt='RightSide_png' style={{width:"317.15px", height:"415.23px",paddingBottom:"45px"}}/>
 		</FirstContainer>
 	);
 };
 
 const FirstContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
 export  default CanvasMobile;
